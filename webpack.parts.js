1b8092df085a8078ae13628905613e4c8c0f6b95
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2017-12-03 22:52:41 +0200

    add autoprefixer

--- webpack.parts.js
+++ webpack.parts.js
@@ -65,3 +65,10 @@ exports.extractCSS = ({include, exclude, use}) => {
     plugins: [plugin],
   };
 };
+
+exports.autoprefix = () => ({
+  loader: 'postcss-loader',
+  options: {
+    plugins: () => [require('autoprefixer')()],
+  },
+});
