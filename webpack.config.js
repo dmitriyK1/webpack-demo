d390124fed6af581050fba7dd8d43fa847f34dbc
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2017-12-14 23:07:08 +0200

    generate module ids based on path (instead of order)

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const glob = require('glob');
+const webpack = require('webpack');
 
 const parts = require('./webpack.parts');
 
@@ -22,6 +23,7 @@ const commonConfig = merge([
     },
     plugins: [
       new HtmlWebpackPlugin({ title: 'Webpack demo' }),
+      new webpack.NamedModulesPlugin(),
     ],
   },
   parts.loadFonts({
